3fbdce90269bbb789d291b0291e8ee07019e61ed
Artmen212 <artem.shashkin.1990@mail.ru>
2022-03-03 10:53:11 +0300

    сервер

--- практика 2/server.js
+++ практика 2/server.js
@@ -1,4 +1,3 @@
-const fs = require("fs");
 
 const http = require("http");
 const requestHandler = (request, response) => {
@@ -7,12 +6,6 @@ console.log("Тип запроса: " + request.method);
 console.log("User-Agent: " + request.headers["user-agent"]);
 console.log("Все заголовки");
 console.log(request.headers);
-fs.readFile("hello.txt", "utf8",
-function(error,data){
-console.log("Асинхронное чтение файла");
-if(error) throw error; // если возникла ошибка
-console.log(data); // выводим считанные данные
-});
 response.end(" vce rabotaet");
 };
 http.createServer(requestHandler).listen(3000);
