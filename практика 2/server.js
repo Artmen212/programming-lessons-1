f693335b7a00faca61e1c4033714c00be430b012
Artmen212 <artem.shashkin.1990@mail.ru>
2022-03-03 10:47:00 +0300

    в единой папке

--- практика 2/server.js
+++ практика 2/server.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 
 const http = require("http");
 const requestHandler = (request, response) => {
@@ -6,7 +7,12 @@ console.log("Тип запроса: " + request.method);
 console.log("User-Agent: " + request.headers["user-agent"]);
 console.log("Все заголовки");
 console.log(request.headers);
-
+fs.readFile("hello.txt", "utf8",
+function(error,data){
+console.log("Асинхронное чтение файла");
+if(error) throw error; // если возникла ошибка
+console.log(data); // выводим считанные данные
+});
 response.end(" vce rabotaet");
 };
 http.createServer(requestHandler).listen(3000);
