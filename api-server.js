7e7e16dc162d86c8e0babff45df16734797d79d2
Artmen212 <artem.shashkin.1990@mail.ru>
2022-04-04 12:09:41 +0300

    api-server

--- api-server.js
+++ api-server.js
@@ -3,12 +3,12 @@ const Path = require('path');
 const Axios = require('axios');
 const http = require("http");
 const fs = require("fs");
-let express = require('express');
-const { path } = require('express/lib/application');
-let app = express();
+//var express = require('express');
+//const { path } = require('express/lib/application');
+//var app = express();
 async function downloadFile () {  
   const url = 'https://jsonplaceholder.typicode.com/todos'
-  const path = Path.resolve(__dirname,'axios' , 'code.json')
+  const path = Path.resolve(__dirname,'test' , 'code.json')
   const writer = Fs.createWriteStream(path)
 
   const response = await Axios({
@@ -22,32 +22,37 @@ async function downloadFile () {
   response.data.pipe(writer)
 
   return new Promise((resolve, reject) => {
-    writer.on('finish', resolve)
-    writer.on('error', reject)  
+    writer.on('finish', resolve(path))
+    writer.on('error', reject())  
   })
 
 }
 
-downloadFile()  
+downloadFile().then(path => {
+    fs.readFile(path, 'utf-8', 
+    function(error, data){
+        if(error) throw error;
+        console.log(data)
+        let result = JSON.parse(data).find(item=> item.id==1 )
+        console.log(typeof(data))
 
-async function loadCollection(url, callback) 
-{
-    fs.readFile('code.json', 'utf8', function(error, data) 
-    {
-    if (error) 
-    {
-    console.log(error);
-} 
-else
- {
-    return callback(JSON.parse(data));
-    }
     });
-    }
+
+})  
+
+//async function loadCollection(url, callback) {
+    //fs.readFile('code.json', 'utf8', function(error ,data) {
+    //if (error) {
+    //console.log(error);
+//} else {
+    //return callback(JSON.parse(data));
+    //}
+//});
+    //}
 
 
-app.get(/a/, function (req, res){
-    res.send('success');
-  });
+//app.get(/a/, function (req, res) {
+    //res.send('success');
+  //});
 
-http.createServer(app).listen(3000);
+//http.createServer(app).listen(3000);
