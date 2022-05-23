ddefa32320c5032b173562682d192a8286820022
Artmen212 <artem.shashkin.1990@mail.ru>
2022-05-23 11:24:43 +0300

    express

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
 app.set('port', process.env.PORT || 3000);
-// пользовательская страница 404
 app.use(function (req, res) {
 res.type('text/plain');
 res.status(404);
-res.send('404 — Не найдено');
+res.send('все робит');
 });
-// пользовательская страница 500
 app.use(function (err, req, res, next) {
 console.error(err.stack);
 res.type('text/plain');
@@ -19,17 +17,26 @@ console.log('Express запущен на http://localhost:' +
 app.get('port') + '; нажмите Ctrl+C для завершения.');
 });
 app.get('/', function (req, res) {
-    res.type('text/plain');
-    res.send('Главная страница');
-    });
-    app.get('/about', function (req, res) {
-    res.type('text/plain');
-    res.send('О главной странице');
-    });
-    // пользовательская страница 404
-    app.use(function (req, res, next) {
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 — Не найдено');
-    });
-    .
+res.type('text/plain');
+res.send('Главная страница');
+});
+app.get('/about', function (req, res) {
+res.type('text/plain');
+res.send('О главной странице');
+});
+// пользовательская страница 404
+app.use(function (req, res, next) {
+res.type('text/plain');
+res.status(404);
+res.send('404 — Не найдено');
+});
+app.get('/about*', function (req, res) {
+// отправляем контент...
+})
+app.get('/about/contact', function (req, res) {
+// отправляем контент...
+})
+app.get('/about/directions', function (req, res) {
+// отправляем контент...
+})
+app.use(express.static(__dirname + '/public'));
